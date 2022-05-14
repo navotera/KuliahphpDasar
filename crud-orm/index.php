620f4d6d0eddfbd815f8ec7446546274afd48cc1
Hendra <navotera@gmail.com>
2022-05-14 10:55:43 +0800

    l

--- crud-orm/index.php
+++ crud-orm/index.php
@@ -7,11 +7,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
     <title>Penggunaan ORM</title>
 </head>
 
 
+<div class="container">
+
+    <div class="row my-5">
+        <div class="col-12">
+            <h1> Selamat datang di Aplikasi Koperasi </h1>
+        </div>
+
+    </div>
+
+
+    <div class="row my-5">
+
+        <div class="col-4">
+            <img src="https://www.continent8.com/wp-content/uploads/2017/10/office-icon.jpg" height="140px">
+        </div>
+        <div class="col-8">
+            Aplikasi ini akan melakukan pencatatan terhadap data anggota dan simpanannya
+        </div>
+
+
+    </div>
+
+
+    <div class="row my-5">
+
+        <div class="col-6"><a href="form/anggota.php"><button class="btn btn-primary"><i class="bi bi-plus-lg"></i> Tambah Anggota</button></a></div>
+        <div class="col-6"></div>
+
+
+
+    </div>
+
+
+
+
+</div>
 
 
 
