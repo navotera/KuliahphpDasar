f408cd8b7cb1f54e8e289dd23470a28f43cf5cfc
Hendra <navotera@gmail.com>
2022-06-10 08:20:49 +0800

    remove folder simpanan

--- crud-ormv2/helpers/autoload.php
+++ crud-ormv2/helpers/autoload.php
@@ -1,14 +1,14 @@
 <?php
 
 //orm
-spl_autoload_register(function ($className) {
-    $file = app_dir() . '\\orm\\' . $className . '.php';
-    $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
-    //echo $file;
-    if (file_exists($file)) {
-        include $file;
-    }
-});
+// spl_autoload_register(function ($className) {
+//     $file = app_dir() . '\\orm\\' . $className . '.php';
+//     $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
+//     //echo $file;
+//     if (file_exists($file)) {
+//         include $file;
+//     }
+// });
 
 
 //core
